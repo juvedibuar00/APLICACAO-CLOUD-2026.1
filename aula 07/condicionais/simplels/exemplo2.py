634bb7922d265c4bc3dc06a00e5bb41022ddffdd
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2026-04-10 21:11:14 -0300

    aula 06

---
+++ aula 07/condicionais/simplels/exemplo2.py
@@ -0,0 +1,11 @@
+
+
+# nome = "Kuâ"
+# if nome == "Anderson":
+#     print(f"Prazer em te conhecer, {nome}")
+
+
+numero = 10
+
+if numero & 1:
+    print("É numero par!....'")
---
+++ aula 07/operadores/operadores.py
@@ -0,0 +1,5 @@
+a = int(input("Selecione um numero? ") )
+
+b = int(input("Selecione um numero? ") )
+
+print(f" Soma: {a + b}; Divisao: {a/b}; Subtração: {a - b}; Multiplicacao: {a*b}; Exponenciação: {a**b}; Resto: {a%b}; Divisao inteiro: {a//b}")
